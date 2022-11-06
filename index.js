ae2134896eae93a1210679cdf623173180394095
Raghvender <raghvender1205@gmail.com>
2022-11-06 22:18:22 +0530

    PATCH API Endpoint to add Profile Details

--- index.js
+++ index.js
@@ -5,13 +5,29 @@ const mongoose = require('mongoose');
 dotenv.config();
 const port = process.env.PORT;
 const userRouter = require('./routes');
+const { User } = require('./models/User');
 const app = express();
 app.use(express.json());
 
 // API Router
 app.use('/api/user', userRouter);
+// Add Profile Data
+app.patch('/api/user/update/:username', async (req, res) => {
+    const { Instagram, Age, Gender, Abstract, Interests, Zodiac, Pets, Company, Drinking, Smoking, PerfectDateQuestion, Quote } = req.body;
+    const { username } = req.params;
+    const filter = { username: username };
 
-// Connect
+    const updatedUser = await User.findOneAndUpdate(filter, req.body, { new: true }).catch(err => {
+        return res.status(400).send(err);
+    });
+
+    return res.status(200).json({
+        message: "Added Details",
+        data: updatedUser
+    });
+});
+
+// Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
 }).then(() => 
     console.log("Connected to Database")
