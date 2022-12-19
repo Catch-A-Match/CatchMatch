18027b190ce86b73cf320d533c93f15a04fdafa6
Raghvender <raghvender1205@gmail.com>
2022-12-19 16:51:06 +0530

    Routes for profile

--- routes.js
+++ routes.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
+const { check } = require('express-validator');
 const { signUp, verifyOtp } = require('./controllers/userController');
+const { createProfile } = require('./controllers/profileController');
 
+// Auth Routes
 router.route('/signup')
     .post(signUp);
 router.route('/signup/verify')
     .post(verifyOtp);
+
+// Profile Routes [Apply check() for required fields]
+router.post('/profiles', [
+    check('Abstract').not().isEmpty(),
+], createProfile)
+
 module.exports = router;
