f5abee7cf1d83b6ca5fcf4c3df16fa9c61535b4f
Raghvender <raghvender1205@gmail.com>
2022-12-19 16:50:12 +0530

    Create a new Profile

---
+++ controllers/profileController.js
@@ -0,0 +1,35 @@
+const { Profile } = require('../models/Profile');
+const { User } = require('../models/User');
+const { check, validationResult } = require('express-validator');
+
+/**
+ * Create a Profile
+ */
+exports.createProfile = (req, res) => {
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
+        return res.status(400).json({ errors: err.array() });
+    }
+
+    const { instagram, Age, Gender, Abstract, interests, zodiac, pets, company, drinking, smoking, perfectdatequestion, quote } = req.body;
+    // New Profile Object
+    const newProfile = new Profile({
+        user: req.user.id,
+        instagram, // Not required
+        Age, 
+        Gender, 
+        Abstract, 
+        interests, 
+        zodiac, 
+        pets, 
+        company, 
+        drinking, 
+        smoking, 
+        perfectdatequestion, 
+        quote  
+    });
+
+    // Save to Database
+    newProfile.save().then(profile => res.json(profile))
+        .catch(err => console.error(err));
+}
