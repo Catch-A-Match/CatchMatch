b8af57d408d730e899d3713326ab71002eab4d57
Raghvender <raghvender1205@gmail.com>
2022-12-24 02:20:27 +0530

    Refactor

--- index.js
+++ index.js
@@ -22,22 +22,6 @@ app.use('/api/profile', profileRoutes);
 app.use('/api/message', messageRoutes);
 app.use('/api/match', matchRoutes);
 
-// Add Profile Data
-app.patch('/api/user/update/:username', async (req, res) => {
-    const { Instagram, Age, Gender, Abstract, Interests, Zodiac, Pets, Company, Drinking, Smoking, PerfectDateQuestion, Quote } = req.body;
-    const { username } = req.params;
-    const filter = { username: username };
-
-    const updatedUser = await User.findOneAndUpdate(filter, req.body, { new: true }).catch(err => {
-        return res.status(400).send(err);
-    });
-
-    return res.status(200).json({
-        message: "Added Details",
-        data: updatedUser
-    });
-});
-
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
 }).then(() => 
