c9b5074ee9d1607375a83f2baea8b5c7cc62971e
Raghvender <raghvender1205@gmail.com>
2022-12-23 22:02:50 +0530

    Fixed Errors and API Routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,10 @@
 const bcrypt = require('bcrypt');
-const _ = require('lodash');
-const axios = require('axios');
 const twilio = require('twilio');
+const jwt = require('jsonwebtoken');
 const otpGenerator = require('otp-generator');
-
+const express = require('express');
+const router = express.Router();
 const client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
-
 const { User } = require('../models/User');
 const { Otp } = require('../models/otp');
 
@@ -26,34 +25,34 @@ function sendOTP(to, otp) {
     });
 }
 
+// Generate JWT for the user
+function generateJWT(user) {
+    return jwt.sign({
+        id: user._id,
+        username: user.username,
+        number: user.number
+    }, process.env.JWT_SECRET, { expiresIn: '3d' });
+}
+
 /**
  * Function to get Number and Send OTP using SMS Twilio
  */
-module.exports.signUp = async (req, res) => {
+router.post('/signUp', async (req, res) => {
     try {
-        const user = await User.findOne({
-            number: req.body.number,
-            username: req.body.username,
-        });
-        const number = req.body.number.toString();
-        const username = req.body.username;
+        const user = await User.findOne({ number: req.body.number })
         if (user) {
-            return res.status(400).json({
-                error: true,
-                message: "Username already in user"
-            });
+            return res.status(400).json({ msg: 'User already exists' });
         }
-        // Randomly Generate OTP
-        // const OTP = Math.floor(100000 + Math.random() * 900000);
         const OTP = otpGenerator.generate(6, {
             digits: true, lowerCaseAlphabets: false, upperCaseAlphabets: false, specialChars: false
         });
         console.log(OTP);
         
-        const otp = new Otp({ number: number, otp: OTP })
         const salt = await bcrypt.genSalt(10);
-        otp.otp = await bcrypt.hash(OTP, salt);
-        const result = await otp.save();
+        const hashedOTP = await bcrypt.hash(OTP, salt);
+        // Save the OTP
+        const otp = new Otp({ number: req.body.number, otp: hashedOTP });
+        await otp.save();
 
         sendOTP(number, OTP);
         return res.status(201).send(" OTP Sent Successfully ");
@@ -64,50 +63,47 @@ module.exports.signUp = async (req, res) => {
             message: "Cannot Sign Up"
         });
     }
-}
+});
 
 /**
  * Function to verify OTP
  */
-module.exports.verifyOtp = async (req, res) => {
-    const otpHolder = await Otp.find({
-        number: req.body.number,
-        username: req.body.username
-    });
-
+router.post('/signup/verify', async (req, res) => {
+    const otpHolder = await Otp.find({ number: req.body.number }).sort({
+        createdAt: -1
+    }).limit(1);
     if (otpHolder.length == 0) {
-        return res.status(400).send("OTP Expired");
+        return res.status(400).json({ msg: "OTP Expired" });
     }
 
-    // Suppose Multiple OTPs are sent, we need the latest one so...
-    const rightOtpFind = otpHolder[otpHolder.length - 1];
-    const validateUser = await bcrypt.compare(req.body.otp, rightOtpFind.otp);
-
-    if (rightOtpFind.number == req.body.number && validateUser) {
-        const user = new User(_.pick(req.body, ['number', 'username']));
-        const token = user.generateJWT();
-        const result = await user.save();
-
-        // When User is assigned, then delete OTP
-        const deleteOtp = await Otp.deleteMany({
-            number: rightOtpFind.number
+    // Compare OTP sent with hashed OTP
+    const validateUser = await bcrypt.compare(req.body.otp, otpHolder[0].otp);
+    if (validateUser) {
+        // Create new User
+        const newUser = new User({
+            number: req.body.number,
+            username: req.body.username,
+            location: req.body.location
         });
-
-        user.save();
-        return res.status(200).send({
-            message: "Your are Registered", 
+        const result = await newUser.save();
+        // Delete OTP from database
+        await Otp.deleteMany({ number: req.body.number });
+        // JWT Token
+        const token = generateJWT(newUser);
+        res.status(200).send({
+            message: "User Registered",
             token: token,
             data: result
         });
     } else {
         return res.status(400).send("Wrong OTP Entered");
     }
-}
+});
 
 /**
  * Function to get Nearby Users
  */
-exports.getNearbyUsers = async (req, res) => {
+router.get('/nearby-users', async (req, res) => {
     try {
         const userId = req.query.userId;
         const radius = req.query.radius;
@@ -119,7 +115,7 @@ exports.getNearbyUsers = async (req, res) => {
     } catch (err) {
         res.status(500).json({ msg: err.message });
     }
-};
+});
 
 /**
  *  Helper Function to get nearby users
@@ -148,4 +144,4 @@ const helper = async (userId, radius, page, limit) => {
     return nearbyUsers;
 }
 
-module.exports = { helper }
+module.exports = router;
